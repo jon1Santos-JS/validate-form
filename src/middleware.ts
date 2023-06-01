d2792934a5406f54f598fea5e815cd7e550b4b60
João Paulo dos Santos <42220755+jon1Santos@users.noreply.github.com>
2023-06-01 11:09:33 -0300

    Update middleware.ts
    test url request

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,7 @@ export default async function handler(req: NextRequest) {
     ADRESSES?.forEach((address) => {
         if (authorization.requestURL === address) authorization.isValid = true;
     });
+    console.log(authorization.requestURL);
     ADRESSES?.forEach((address) => {
         AUTHORIZED_PAGE_ROUTES.forEach((route) => {
             if (authorization.requestURL + route === address + route)
