d78e1bf3ec822ab4f70e8377513a1b73ab9933c9
jon1-santos <gupa581333@gmail.com>
2024-01-03 18:05:24 -0300

    fixing loading user image

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,20 +1,12 @@
-import { ReactNode } from 'react';
-
 type LoadingSpinnerProps = {
     isOpen: boolean;
-    children: ReactNode;
 };
 
-export default function LoadingSpinner({
-    isOpen,
-    children,
-}: LoadingSpinnerProps) {
+export default function LoadingSpinner({ isOpen }: LoadingSpinnerProps) {
     return (
         <>
             <div className={`${isOpen ? 'o-spinner-container' : ''}`}>
-                <div className={`${isOpen ? 'spinner-element' : ''}`}>
-                    {children}
-                </div>
+                <div className={`${isOpen ? 'spinner-element' : ''}`}></div>
             </div>
         </>
     );
