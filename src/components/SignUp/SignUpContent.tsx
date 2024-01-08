66a2806b14f69fdfa202d8b398bd047465cba466
jon1-santos <gupa581333@gmail.com>
2024-01-08 17:51:15 -0300

    remove handle modal state

--- src/components/SignUp/SignUpContent.tsx
+++ src/components/SignUp/SignUpContent.tsx
@@ -1,20 +1,9 @@
-import { useState } from 'react';
 import SignUpForm from './SignUpForm';
-import SignUpModal from './SignUpModal';
 
 export default function SignUpContent() {
-    const [modalState, setModalState] = useState(false); // SHOW MODAL
     return (
         <>
-            <SignUpForm
-                ownProps={{
-                    setModalState,
-                }}
-            />
-            <SignUpModal
-                isModalOpen={modalState}
-                onCloseModal={() => setModalState(false)}
-            />
+            <SignUpForm />
         </>
     );
 }
